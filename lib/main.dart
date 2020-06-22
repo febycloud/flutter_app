d8dde9b598faac1e5cba961f6f493150f56f0c12
Fyun <fei.yun.unhi@gmail.com>
2020-06-22 19:56:38 -0400

    add update

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+void main()=>runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  //root
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('My app now'),
-            centerTitle: true,
-          ),
-          body: Center(
-            child: new Column(
-              children:<Widget>[
-                new Text('Hello baby'),
-                new RaisedButton(
-                    child: Text("normal"),
-                    onPressed: () {print('active');},
-                  ),
-              ]
-          )),
-          floatingActionButton: FloatingActionButton(
-            child: Text('Press me'),
-            onPressed: () {
-              print('Hi there');
-            },
-          )
-        )
-
+      title: 'Chat flutter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+        accentColor: Color(0xFFFEF9EB),
+      ),
     );
   }
 }
-
 
 
